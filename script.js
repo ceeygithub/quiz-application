1707b6689bd140fca5f97b16a244178562fc2200
ceeygithub <cynthiaekhomwandolor@gmail.com>
2024-01-04 04:44:47 +0100

    added light and dark theme log

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // dark theme
 let icon = document.querySelector('.moon');
+let logo = document.querySelector('.logo');
 // let timerIcon = document.querySelector('.timer-icon');
 
 icon.addEventListener('click', () => {
@@ -8,12 +9,14 @@ icon.addEventListener('click', () => {
 
         icon.className = "fa-solid fa-sun sun"
         icon.style.color = 'white';
+        logo.src = "./assets/Group 37018 (1).png";
         // timerIcon.style.color = 'white';
 
     }
     else {
         icon.className = "fa-solid fa-moon moon";
         icon.style.color = 'rgb(61,119,247)';
+        logo.src = "./assets/Group 37018.png";
         // timerIcon.style.color = 'black';
     }
 })
